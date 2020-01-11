88d3a7419dbcc378559cf691a5def525f304d194
StepixCode <suchanek.stepan1@gmail.com>
2020-01-11 18:21:12 +0100

    Responzivita product layoutu
    -responzivita levé navigace
    -řádek s buttonama nad layoutem
    -laďění

--- js/Checkbox.js
+++ js/Checkbox.js
@@ -1,9 +1,41 @@
 
-function resetCheckbox(number)
+$(document).ready(function(){
+    $(".responsive-filter-btn").click(function(){
+        responsiveLayout();
+    });
+    $(".responsive-close-left").click(function(){
+        responsiveLayout();
+    });
+
+    $(window).resize(function() {
+        var win =  $(window).width(); 
+        if (win >= 992 && $("#left-layout-col").hasClass("layout-show")) 
+        { 
+            responsiveLayout();
+        }
+    });
+
+
+//Zobrazí / skryje responzivní leftMenu u produkt layoutu
+function responsiveLayout(){
+    $("#left-layout-col").toggleClass("layout-show");
+    $("#left-layout-col").toggleClass("d-none");
+    $("#left-layout-col").toggleClass("col-4");
+    $(".responsive-close-left").toggleClass("d-none");
+    $(".product-layout-aside").toggleClass("col-12");
+}
+
+
+
+
+
+
+});
+function resetCheckbox(number,boxName)
 {
-    for(var i = 0; i < document.getElementsByClassName("submenu-checkbox").length; i++){
+    for(var i = 0; i < document.getElementsByClassName(boxName).length; i++){
         if(i != number)
-            document.getElementsByClassName("submenu-checkbox")[i].checked = false;
+            document.getElementsByClassName(boxName)[i].checked = false;
 
     }
 }
