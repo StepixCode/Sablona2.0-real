37d69af0754fac15728c22926654a79221daa11b
StepixCode <suchanek.stepan1@gmail.com>
2020-01-12 11:47:48 +0100

    Změna typu layoutu
    -tímto je hotov productLayout (není laďěn)

--- js/Checkbox.js
+++ js/Checkbox.js
@@ -1,18 +1,27 @@
 
 $(document).ready(function(){
+    
+    $(".layout-grid").addClass("active");
+
+    //#region  Reszponzivní filtry
     $(".responsive-filter-btn").click(function(){
         responsiveLayout();
     });
     $(".responsive-close-left").click(function(){
         responsiveLayout();
     });
+    //#endregion
 
     $(window).resize(function() {
+       
+        //#region  Responzivní filtry
         var win =  $(window).width(); 
         if (win >= 992 && $("#left-layout-col").hasClass("layout-show")) 
         { 
             responsiveLayout();
         }
+        //#endregion
+    
     });
 
 
@@ -25,12 +34,29 @@ function responsiveLayout(){
     $(".product-layout-aside").toggleClass("col-12");
 }
 
-
-
-
+//#region Typ layoutu
+$(".layout-grid").click(function(){
+    $(".product-layout-row").addClass("three-product-row");
+    $(".product-layout-row").removeClass("list-product-row");
+    $('.product').attr('class','col-12 col-md-6 col-lg-4 pr-0 pr-lg-3 pr-md-3 product');
+    $(this).addClass("active");
+    $(".layout-list").removeClass("active");
+});
+$(".layout-list").click(function(){
+    $(".product-layout-row").removeClass("three-product-row");
+    $(".product-layout-row").addClass("list-product-row");
+    $('.product').attr('class','product');
+    $('.product').addClass('col-12');
+    $(this).addClass("active");
+    $(".layout-grid").removeClass("active");
+});
+//#endregion
 
 
 });
+
+
+//Pokud je někde víc checkboxů jedné classy bude aktviní jen jeden z nich
 function resetCheckbox(number,boxName)
 {
     for(var i = 0; i < document.getElementsByClassName(boxName).length; i++){
