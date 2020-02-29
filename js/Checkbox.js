4e104cf3b3d7fe73524d9734e2997927bcb9698b
StepixCode <suchanek.stepan1@gmail.com>
2020-02-29 13:14:52 +0100

    Alt product detail
    -alt verze productDetailu
    -opraven carousel hodnocení na productDetailu

--- js/Checkbox.js
+++ js/Checkbox.js
@@ -1,6 +1,5 @@
 
 $(document).ready(function(){
-    var defaultLayout;
     $(".layout-grid").addClass("active");
 
     //#region  Reszponzivní filtry
@@ -99,4 +98,31 @@ function checkCheckboxes(boxName,checkFalse = true)
     }
 
 
+}
+
+function productDetailCarousel(){
+    $('.reviews-carousel').owlCarousel({
+        loop:false,
+        margin:10,
+        responsiveClass:true,
+        autoHeight:true,
+        nav: true,
+        navText: [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        dots: true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:1,
+                loop:false,
+            }
+        }
+    })
 }
